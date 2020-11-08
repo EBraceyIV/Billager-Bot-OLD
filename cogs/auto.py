1b34cf1ad7c64b78eea1402b9b51e30b87f1ab8f
EBraceyIV <dbraceyiv@me.com>
2020-11-07 20:06:15 -0500

    Adjusted the index of the sorted scores used to make the callout, apparently 0 was not the lowest score index. Increased the sleep time from 5 seconds to 120. Added comments overall to callout post listener.

--- cogs/auto.py
+++ cogs/auto.py
@@ -26,29 +26,39 @@ class Auto(commands.Cog):
     # Every Friday night, make a call out post about whoever got the lowest score that week
     @commands.Cog.listener()
     async def on_message(self, message):
+        # Global variable to keep track of whether or not the callout post has been made yet out of the scope of this
+        # listener so that it does not trigger multiple times
         global called_out
 
         # Do not reply to BB's own messages
         if message.author == self.bot.user:
             return
 
+        # The day and hour of the message being sent is needed to trigger the callout post
         time = datetime.datetime.now()
+
         if not called_out:
+            # The callout post can be triggered on Fridays between 5 and 7 P.M. EST in a specific channel
             if time.day == 6 and time.hour in [17, 18, 19] and message.channel.id == 743616007435976754:
+                # Wait a short while after detecting a valid trigger message to make it seem more organic, but only
+                # after setting called_out to True so that no other messages trigger another post during the wait
                 called_out = True
-                await asyncio.sleep(5)
-                plusMinus = shelve.open("plusMinus")
+                await asyncio.sleep(120)
+
                 # Sort the current user scores from highest to lowest
+                plusMinus = shelve.open("plusMinus")
                 score_sorted = sorted(plusMinus.items(), key=lambda x: x[1])
                 plusMinus.close()
-                await message.channel.send("This is your weekly Bad Score Callout Post, a public service brought to you by "
-                                           "Billager Bot. \n"
-                                           "This week, " + str(score_sorted[0][0]) + " has the worst score so far. All the "
-                                           "way down at a fat " + str(score_sorted[0][1]) + "!")
+
+                # Send our fun little message letting our friend know they should try making better jokes
+                await message.channel.send("This is your weekly Bad Score Callout Post, a public service brought to "
+                                           "you by Billager Bot. \n"
+                                           "This week, " + str(score_sorted[-1][0]) + " has the worst score so far. "
+                                           "All the way down at a fat " + str(score_sorted[-1][1]) + "!")
         else:
+            # Reset the called_out status on Thursday
             if called_out and time.day == 5:
                 called_out = False
-
 
 
 def setup(bot):
