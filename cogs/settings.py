b0ac4274fa9d6cb01080e99829cd9273e9a6c612
EBraceyIV <dbraceyiv@me.com>
2021-03-13 22:06:31 -0500

    Preliminary work on storing some settings as JSON data

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,24 +1,29 @@
 from discord.ext import commands
+import json
 
 
 class settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Setting up some framework for a more modular way of defining which channels certain outputs go to
+    #
+    # Dealing with reading and writing to JSON files for settings
+    @commands.command(name="calloutChannel", hidden=True)
+    async def calloutChannel(self, ctx, channel):
+        with open("callout.json", "w") as json_file:
+            json.dump({"callout_channel": channel}, json_file)
+
+    @commands.command(name="json", hidden=True)
+    async def json(self, ctx):
+        with open("callout.json") as f:
+            print(json.load(f))
+
     # A more advanced method to fixing any issues with the voice chat functionality, reloads the whole cog
     @commands.command(name="vreload", hidden=True)
     async def vreload(self, ctx):
         self.bot.reload_extension("cogs.voice")
         await ctx.send("Reloaded voice cog.")
-
-
-'''
-  @commands.command()
-async def score_reply(self, ctx, channel: discord.TextChannel):
-    print(channel)
-    score_channel = channel
-    await ctx.send('Score outputs will be sent to ' + str(score_channel))
-'''
 
 
 def setup(bot):
