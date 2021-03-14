9afe3cd473665e0c88bb2e4f78f4a1481c71db06
EBraceyIV <dbraceyiv@me.com>
2021-03-14 13:32:37 -0400

    Modification to the JSON settings commands.

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,22 +1,26 @@
 from discord.ext import commands
 import json
 
+validOutputs = ["callout"]
 
-class settings(commands.Cog):
+
+class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     # Setting up some framework for a more modular way of defining which channels certain outputs go to
     #
     # Dealing with reading and writing to JSON files for settings
-    @commands.command(name="calloutChannel", hidden=True)
-    async def calloutChannel(self, ctx, channel):
-        with open("callout.json", "w") as json_file:
-            json.dump({"callout_channel": channel}, json_file)
+    @commands.command(name="outputs", hidden=True)
+    async def outputs(self, ctx, output, channel):
+        if output not in validOutputs:
+            await ctx.send("That is not a valid output label. Valid output labels are: " + str(validOutputs))
+        with open("output.json", "w") as outputJSON:
+            json.dump({output: channel}, outputJSON)
 
-    @commands.command(name="json", hidden=True)
+    @commands.command(name="consoleJSON", hidden=True)
     async def json(self, ctx):
-        with open("callout.json") as f:
+        with open("output.json") as f:
             print(json.load(f))
 
     # A more advanced method to fixing any issues with the voice chat functionality, reloads the whole cog
@@ -25,6 +29,11 @@ class settings(commands.Cog):
         self.bot.reload_extension("cogs.voice")
         await ctx.send("Reloaded voice cog.")
 
+    @outputs.error
+    async def score_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You're missing the output label or the designated channel, try again.")
+
 
 def setup(bot):
-    bot.add_cog(settings(bot))
+    bot.add_cog(Settings(bot))
